c7890c62ff00e66a1657e582fc3d0640b920259d
Sean Hinchee <henesy.dev@gmail.com>
2018-03-01 18:58:02 -0600

    added tuple class

--- workspace/PA1/src/Tuple.java
+++ workspace/PA1/src/Tuple.java
@@ -1,4 +1,27 @@
 
 public class Tuple {
-
+	Integer key;
+	String value;
+	
+	// Basic constructor
+	Tuple(int keyP, String valueP){
+		key = keyP;
+		value = valueP;
+	}
+	
+	// Returns key
+	Integer getKey(){
+		return key;
+	}
+	
+	// Returns value
+	String getValue(){
+		return value;
+	}
+	
+	// Basic comparator
+	Boolean equals(Tuple t){
+		return this.key.equals(t.key) && this.value.equals(t.value);
+	}
+	
 }
