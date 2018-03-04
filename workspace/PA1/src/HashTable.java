1ee7afe007e0caf52314b06e8391d2fd4d231726
Tyler Fenton <tjfenton@iastate.edu>
2018-03-04 01:05:40 -0600

    Worked on generating next largest prime for HashTable

--- workspace/PA1/src/HashTable.java
+++ workspace/PA1/src/HashTable.java
@@ -67,4 +67,24 @@ public class HashTable {
 			
 		}
 
+		public static boolean isPrime(int n) {
+			for(int i= 2; i<=Math.sqrt(n); i++)
+				if (n%i==0)
+					return false;
+			return true;
+		}
+
+		// Generages the next largest prime given a limit
+		public static int getNextPrime(int given_limit) {
+			int limit, i, j;
+			// Bertrand's Postulate states that when given_limit > 3 next prime exists within
+			// given_limit < p < (given_limit * 2) - 2
+			if (given_limit > 3) {
+				limit = (given_limit * 2) - 2;
+			} else {
+				return Math.ceil()
+			}
+
+		}
+
 }
